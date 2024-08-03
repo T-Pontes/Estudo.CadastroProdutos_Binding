5e22c3364daa550d0f4ef7bdd809e74beca7bed9
Tiago Pontes <76593718+T-Pontes@users.noreply.github.com>
2024-08-02 23:52:14 -0300

    Criação dos Mappers de Etidade para DTO e vice-versa

---
+++ Infra.Adapters/Mappings/CategoryMapping.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities;
+using Core.Enums;
+using Infra.Adapters.DTOs;
+
+namespace Infra.Adapters.Mappings;
+
+public static class CategoryMapping
+{
+    /// <summary> Método de mapeamento de "Entidades" para "DTOs". </summary>
+    /// /// <param name="obj"> Parametro obrigatório sem valor padrão. </param>
+    public static CategoryDTO EntityToDTO(Category obj)
+    {
+        return new CategoryDTO()
+        {
+            Id = obj.Id,
+            CategoryName = obj.CategoryName,
+            CategoryDescription = obj.CategoryDescription,
+            CategoryStatus = obj.CategoryStatus == EStatus.Active ? true : false,
+        };
+    }
+
+    /// <summary> Método de mapeamento de "DTOs" para "Entidades". </summary>
+    /// /// <param name="obj"> Parametro obrigatório sem valor padrão. </param>
+    public static Category DTOToEntity(CategoryDTO obj)
+    {
+        return new Category(
+            obj.CategoryName,
+            obj.CategoryDescription,
+            obj.CategoryStatus == true ? EStatus.Active : EStatus.Inactive,
+            obj.Id
+            );
+    }
+}
---
+++ Infra.Adapters/Mappings/ProductMapping.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using Core.Enums;
+using Infra.Adapters.DTOs;
+
+namespace Infra.Adapters.Mappings;
+
+public class ProductMapping
+{
+    /// <summary> Método de mapeamento de "Entidades" para "DTOs". </summary>
+    /// /// <param name="obj"> Parametro obrigatório sem valor padrão. </param>
+    public static ProductDTO EntityToDTO(Product obj)
+    {
+        return new ProductDTO()
+        {
+            Id = obj.Id,
+            ProductName = obj.ProductName,
+            ProductDescription = obj.ProductDescription,
+            Price = obj.Price.ToString(),
+            Category = CategoryMapping.EntityToDTO(obj.Category),
+        };
+    }
+
+    /// <summary> Método de mapeamento de "DTOs" para "Entidades". </summary>
+    /// /// <param name="obj"> Parametro obrigatório sem valor padrão. </param>
+    public static Product DTOToEntity(ProductDTO obj)
+    {
+        return new Product(
+            obj.ProductName,
+            Convert.ToDouble(obj.Price),
+            CategoryMapping.DTOToEntity(obj.Category),
+            obj.ProductDescription,
+            obj.ProductStatus == true ? EStatus.Active : EStatus.Inactive,
+            obj.Id
+            );
+    }
+}
