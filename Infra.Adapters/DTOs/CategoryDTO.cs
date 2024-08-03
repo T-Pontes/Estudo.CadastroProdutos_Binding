358209a7dde0ddd9c9cf5ecd5afc5bdac81cf861
Tiago Pontes <76593718+T-Pontes@users.noreply.github.com>
2024-08-02 23:51:03 -0300

    Criação dos DTOs "CategoriaDTO" e "ProdutoDTO" para transferência de informações

---
+++ Infra.Adapters/DTOs/CategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Infra.Adapters.DTOs;
+
+/// <summary> Classe responsável pela tranferência de valores de propriedades da entidade "Categoria" sem expô-la. </summary>
+public class CategoryDTO
+{
+    public int Id { get; set; }
+    public string CategoryName { get; set; } = null!;
+    public string? CategoryDescription { get; set; }
+    public bool CategoryStatus { get; set; }
+}
---
+++ Infra.Adapters/DTOs/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Infra.Adapters.DTOs;
+
+/// <summary> Classe responsável pela tranferência de valores de propriedades da entidade "Produto" sem expô-la. </summary>
+public class ProductDTO
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; } = null!;
+    public string? ProductDescription { get; set; }
+    public string Price { get; set; } = null!;
+    public bool ProductStatus { get; set; }
+
+    public CategoryDTO Category { get; set; } = null!;
+}
