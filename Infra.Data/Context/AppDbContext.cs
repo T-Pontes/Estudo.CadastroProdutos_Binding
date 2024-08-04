0c7b46b009a6c981bb99a00050ff9375ae7f2a38
Tiago Pontes <76593718+T-Pontes@users.noreply.github.com>
2024-08-04 15:35:34 -0300

    Criação da classe de contexto da aplicação no projeto "Infra.Data"

---
+++ Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Context;
+
+/// <summary>
+/// Classe responsável por fornecer a base para uma instância de contexto de bando de dados
+/// </summary>
+/// <param name="optionsBuilder"> Parâmetro que contém as configurações de acesso ao banco de dados </param>
+public class AppDbContext(DbContextOptionsBuilder optionsBuilder) : DbContext(optionsBuilder.Options)
+{
+    /// <summary>
+    /// Coleção de objetos de "Category" restreados no contexto
+    /// </summary>
+    public DbSet<Category> Categories { get; set; }
+
+    /// <summary>
+    /// Coleção de objetos de "Product" restreados no contexto
+    /// </summary>
+    public DbSet<Product> Products { get; set; }
+}
