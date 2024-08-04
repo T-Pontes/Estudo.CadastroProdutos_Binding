e62fad918851276197eb020e1df3b0a3663c28ce
Tiago Pontes <76593718+T-Pontes@users.noreply.github.com>
2024-08-04 16:49:06 -0300

    Configuração do contexto e realização da primeira migração

--- Infra.Data/Context/AppDbContext.cs
+++ Infra.Data/Context/AppDbContext.cs
@@ -7,8 +7,18 @@ namespace Infra.Data.Context;
 /// Classe responsável por fornecer a base para uma instância de contexto de bando de dados
 /// </summary>
 /// <param name="optionsBuilder"> Parâmetro que contém as configurações de acesso ao banco de dados </param>
-public class AppDbContext(DbContextOptionsBuilder optionsBuilder) : DbContext(optionsBuilder.Options)
+public class AppDbContext : DbContext
 {
+    /// <summary>
+    /// Este método é responsável por configurar a instância do contexto para representar o banco de dados
+    /// </summary>
+    /// <param name="optionsBuilder"> Parâmentros de configuração do contexto</param>
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // A configuraçao da string de conexão deverá ser alocada em arquivo de configuração com essa finalidade
+        optionsBuilder.UseSqlServer("Data Source=LAPTOP-H21GT0EC\\SQLEXPRESS;Initial Catalog=CadProdDB;Integrated Security=True;Trust Server Certificate=True");
+    }
+
     /// <summary>
     /// Coleção de objetos de "Category" restreados no contexto
     /// </summary>
